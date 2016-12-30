174a7e4a21818287ebd933b041055cc6cece1569
mathuramg <mmg9291@gmail.com>
2016-12-30 02:16:00 -0500

    add live data collection

---
+++ LiveData/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  //line graph
+  // makeAjaxCallLineGraph();
+
+
+});
+
+function myFunction() {
+  console.log(document.getElementById('myText').value);
+  addEveryMinute(document.getElementById('myText').value);
+}
