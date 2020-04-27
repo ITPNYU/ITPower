e7774fd7bf05547b5c69e6cec2c4a185c479149d
Yeseul Song <yeseul.song@nyu.edu>
2020-04-27 17:31:06 -0400

    Create arduino_secrets.h

---
+++ WeatherStation/GetData/GetWeatherStationData/arduino_secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "" // enter your wifi ssid
+#define SECRET_PASS "" // enter your wifi password
+#define MAC_ID "" // enter weather station's mac id
+#define SESSION_KEY "" // enter weather station's session key
